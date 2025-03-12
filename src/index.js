bc2f8e4dff115cc146dd22a8892e688426d504e1
Mili Martinez <jmin13tz@gmail.com>
2025-03-12 08:38:34 -0600

    subi mi api a github

---
+++ src/index.js
@@ -0,0 +1,48 @@
+//ARCHIVO index.js
+
+const express = require('express');
+const app = express();
+const morgan=require('morgan');
+ 
+//Configuraciones
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2)
+ 
+//Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+ 
+//Nuestro primer WS Get
+app.get('/', (req, res) => {    
+    res.json(
+        {
+            "Title": "Hola soy Mili y esta es mi primer API"
+        }
+    );
+})
+ 
+
+//End point para sumar dos números 
+
+
+// Endpoint para sumar dos números
+app.post('/sumar', (req, res) => {
+    const { num1, num2 } = req.body;
+
+    // Validar que se hayan enviado los dos números y que no estén vacíos
+    if (!num1 || !num2) {
+        return res.status(400).send({ error: 'Faltan números para sumar' });
+    }
+
+    // Sumar los números
+    const resultado = num1 + num2;
+
+    // Enviar el resultado
+    res.send({ resultado });
+});
+
+//Iniciando el servidor
+app.listen(app.get('port'),()=>{
+    console.log(`Server listening on port ${app.get('port')}`);
+});
